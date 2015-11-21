831b37eb7361071cbbff0d160b91f9f12fc87030
xuxiangyang <xxy@creatingev.com>
2015-11-21 10:12:11 +0800

    update packages

--- emacs.d/elpa/dash-20150804.1354/dash-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/elpa/dash-20150804.1354/dash-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/elpa/dash-20150804.1354/dash.el")
-;;;;;;  (21972 29204 620387 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
