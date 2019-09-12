1cbbee562f772da2ff28499e2f0156031a672ae7
xxy <xxy@xuxiangyang.com>
2019-09-12 14:23:00 +0800

    sync emacs

--- emacs.d/elpa/python-mode-20190910.1943/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20190910.1943" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ emacs.d/elpa/python-mode-20190911.1843/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20190911.1843" "Python major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
